0ebda44543dce5c814cfb0a76eaa2300e230985a
LoicLeJeune <43613467+LoicLeJeune@users.noreply.github.com>
2018-09-26 17:28:58 +0200

    Update viewDetail.php
    Séparation code php/html , mise en page

--- view/viewDetail.php
+++ view/viewDetail.php
@@ -1,13 +1,55 @@
 <?php
 include "header.php";
-if (isset($result)) {
-    if (filter_var($result["id"], FILTER_VALIDATE_INT)) {
-        echo "Prenom : " . $result["prenom"] . "<BR>Nom : " . $result["nom"] . "<BR>Telephone : " . $result["tel"] . "<BR>BIO : " . $result["bio"] . "<BR>";
-    }
-} else {
+
+if (isset($result)){
+    if (filter_var($result["id"],FILTER_VALIDATE_INT)){
+?>
+<div class="row">
+    <div class="col">
+        Prenom :
+    </div>
+    <div class="col">  
+    <?=$result["prenom"];?>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+Nom :
+</div>
+    <div class="col"> 
+    <?=$result["nom"];?>
+</div>
+</div>
+<div class="row">
+    <div class="col">
+Tel : 
+</div>
+    <div class="col"> 
+    <?=$result["tel"];?>
+</div>
+</div>
+<div class="row">
+    <div class="col">
+Bio : 
+</div>
+    <div class="col"> 
+    <?=$result["bio"];?>
+    </div>
+</div>
+<br>
+<div><a href="<?=$_SESSION['chemin'];?>" class="btn btn-primary">Retour à L'index</a></div>
+
+
+<?php 
+
+    } 
+} 
+else {
     http_response_code(404);
     header('Location: index.php');
     exit;
 }
-echo "<a href=\"".$_SESSION['chemin']."\" class=\"btn btn-primary\">Retour à L'index</a>";
-include "footer.php";
+
+include "footer.php"; 
+
+?>
