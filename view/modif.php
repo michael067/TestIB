18212150908f6fdb96f78e50cc85f4aa4c11bca3
LoicLeJeune <43613467+LoicLeJeune@users.noreply.github.com>
2018-09-26 17:25:16 +0200

    Update modif.php
    Clean code - mise en page + séparation code php/html

--- view/modif.php
+++ view/modif.php
@@ -1,19 +1,54 @@
-<?php
+<?php 
 include "header.php";
+
 if (isset($result)){
     if (filter_var($result["id"],FILTER_VALIDATE_INT)){
-        echo '<form action="'.$_SESSION['chemin'].'/edit" method="post">';
-        echo '<input name="id" type=hidden readonly value="'.$result["id"] . '"><BR>';
-        echo 'Prenom : <input name="prenom" type="text" value="' . $result["prenom"] . '"><BR>';
-        echo 'Nom : <input name="nom" type="text" value="' . $result["nom"] . '"><BR>';
-        echo 'Tel : <input name="tel" type="text" value="' . $result["tel"] . '"><BR>';
-        echo 'Bio : <textarea name="bio">' . $result["bio"] . '</textarea><BR>';
-        echo '<input type="submit" value="GO GO GO"></form>';
-    } 
+?>
+<form action="'.$_SESSION['chemin'].'/edit" method="post">
+<input name="id" type=hidden readonly value="<?=$result["id"];?>">
+<div class="row">
+    <div class="col">
+        Prenom :
+    </div>
+    <div class="col">  
+     <input name="prenom" type="text" value="<?=$result["prenom"];?>"><BR>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+Nom :
+</div>
+    <div class="col"> 
+     <input name="nom" type="text" value="<?=$result["nom"];?>"><BR>
+</div>
+</div>
+<div class="row">
+    <div class="col">
+Tel : 
+</div>
+    <div class="col"> 
+<input name="tel" type="text" value="<?=$result["tel"];?>"><BR>
+</div>
+</div>
+<div class="row">
+    <div class="col">
+Bio : 
+</div>
+    <div class="col"> 
+    <textarea name="bio"><?=$result["bio"];?></textarea><BR>
+</div>
+</div>
+<input type="submit" value="Update">
+</form>
+<a href="/formation/codeAnnuaire/index.php">Retour à L'index</a>
+
+<?php
+} 
 } else {
     http_response_code(404);
     header('Location: index.php');
     exit;
 }
-echo "<a href=\"".$_SESSION['chemin']."\">Retour à L'index</a>";
+
 include "footer.php";
+?>
